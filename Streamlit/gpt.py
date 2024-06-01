0f08680deb03abd95435fec58cb67eb599d2675b
Wonhyeok316 <choewonhyeog3@gmail.com>
2024-06-02 01:16:44 +0900

    New

--- Streamlit/gpt.py
+++ Streamlit/gpt.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import faiss
 import openai
-import time
 from dotenv import load_dotenv, find_dotenv
 import streamlit as st
 from langdetect import detect
@@ -22,7 +21,7 @@ openai.api_key = api_key
 
 # 현재 디렉토리 설정
 current_dir = os.getcwd()
-vector_db_dir = os.path.join(current_dir, '../VectorDB')
+vector_db_dir = os.path.join(current_dir, '..\VectorDB')
 
 # FAISS 인덱스 로드
 index = faiss.read_index(os.path.join(vector_db_dir, 'vector_db.index'))
@@ -38,6 +37,47 @@ for path in file_paths:
     with open(path, 'r', encoding='utf-8') as file:
         documents.append(file.read())
 
+# 파일 경로와 링크 URL 매핑
+file_path_to_url = {
+    r'..\Data\ER32217 재학생장학금 지급지침.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawFullView.do?SEQ=199&SEQ_HISTORY=526',
+    r'..\Data\FR00601 보건및기타.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=76&SEQ_HISTORY=996&PAGE_MODE=1&LAWGROUP=1&TREE_MODE=0',
+    r'..\Data\FR00601 취업규칙_당직.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=76&SEQ_HISTORY=996&PAGE_MODE=1&LAWGROUP=1&TREE_MODE=0',
+    r'..\Data\FR00601 취업규칙_복무.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=76&SEQ_HISTORY=996&PAGE_MODE=1&LAWGROUP=1&TREE_MODE=0',
+    r'..\Data\FR00601 취업규칙_복무_2.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=76&SEQ_HISTORY=996&PAGE_MODE=1&LAWGROUP=1&TREE_MODE=0',
+    r'..\Data\FR00601 취업규칙_복무_3.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=76&SEQ_HISTORY=996&PAGE_MODE=1&LAWGROUP=1&TREE_MODE=0',
+    r'..\Data\FR00601 취업규칙_시간외야간r및휴일근무.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=76&SEQ_HISTORY=996&PAGE_MODE=1&LAWGROUP=1&TREE_MODE=0',
+    r'..\Data\FR00601 취업규칙_임용및퇴직 복지후생.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=76&SEQ_HISTORY=996&PAGE_MODE=1&LAWGROUP=1&TREE_MODE=0',
+    r'..\Data\FR00601 취업규칙_총칙.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=76&SEQ_HISTORY=996&PAGE_MODE=1&LAWGROUP=1&TREE_MODE=0',
+    r'..\Data\FR00601 취업규칙_출장및파견 전보및사무인계.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=76&SEQ_HISTORY=996&PAGE_MODE=1&LAWGROUP=1&TREE_MODE=0',
+    r'..\Data\FR00602 여비규칙_1.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=77&LAWGROUP=1&PAGE=1&SEQ_HISTORY=0',
+    r'..\Data\FR00602 여비규칙_국내출장비.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawFullView.do?SEQ=77&SEQ_HISTORY=987',
+    r'..\Data\FR00602 여비규칙_국내출장비_2.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=77&LAWGROUP=1&PAGE=1&SEQ_HISTORY=0',
+    r'..\Data\FR00602 여비규칙_국외출장비.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail_areaC.do?SEQ=77&LAWGROUP=1&PAGE=1&SEQ_HISTORY=0',
+    r'..\Data\FR00602 여비규칙_국외출장비_2.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=77&LAWGROUP=1&PAGE=1',
+    r'..\Data\FR00602 여비규칙_보칙.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=77&LAWGROUP=1&PAGE=1',
+    r'..\Data\FR00807 물품구매요령_계약의 이행.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=105&LAWGROUP=1&PAGE=1',
+    r'..\Data\FR00807 물품구매요령_계약의 체결.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=105&LAWGROUP=1&PAGE=1',
+    r'..\Data\FR00807 물품구매요령_계약의 체결_2.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=105&LAWGROUP=1&PAGE=1',
+    r'..\Data\FR00807 물품구매요령_구매절차 및 방법.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=105&LAWGROUP=1&PAGE=1',
+    r'..\Data\FR00807 물품구매요령_구매절차 및 방법_2.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=105&LAWGROUP=1&PAGE=1',
+    r'..\Data\FR00807 물품구매요령_기타.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=105&LAWGROUP=1&PAGE=1',
+    r'..\Data\FR00807 물품구매요령_총칙.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=105&LAWGROUP=1&PAGE=1',
+    r'..\Data\국내여비 정액표.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=77&LAWGROUP=1&PAGE=1',
+    r'..\Data\국내이전비 정액표.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=77&LAWGROUP=1&PAGE=1',
+    r'..\Data\국외여비 정액표.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=77&LAWGROUP=1&PAGE=1',
+    r'..\Data\국외이전비 정액표.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=77&LAWGROUP=1&PAGE=1',
+    r'..\Data\장기체제자 월액표.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawDetail.do?SEQ=77&LAWGROUP=1&PAGE=1',
+    r'..\Data\기계공학부_학위취득절차_박사과정.txt': 'https://me.gist.ac.kr/prog/bbsArticle/BBSMSTR_000000000339/view.do',
+    r'..\Data\기계공학부_학위취득절차_박사과정2.txt': 'https://me.gist.ac.kr/prog/bbsArticle/BBSMSTR_000000000339/view.do',
+    r'..\Data\기계공학부_학위취득절차_석사과정.txt': 'https://me.gist.ac.kr/prog/bbsArticle/BBSMSTR_000000000339/view.do',
+    r'..\Data\지구환경공학부_학위취득절차_': 'https://env1.gist.ac.kr/prog/bbsArticle/BBSMSTR_000000000400/view.do',
+    r'..\Data\화학과_학위취득절차_박사.txt': 'https://chem.gist.ac.kr/thumbnail/html/viewer/BBS_202102191216481360.hwp/document.html',
+    r'..\Data\광주과학기술원_학위수여규정_1.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawFullView.do?SEQ=64&SEQ_HISTORY=1076',
+    r'..\Data\광주과학기술원_학위수여규정_2.txt': 'https://law.gist.ac.kr/lmxsrv/law/lawFullView.do?SEQ=64&SEQ_HISTORY=1076'
+    # 필요한 다른 파일들도 여기에 추가
+}
+
+
 # 임베딩 생성 함수
 def get_embedding(text):
     response = openai.Embedding.create(
@@ -46,28 +86,31 @@ def get_embedding(text):
     )
     return np.array(response['data'][0]['embedding'], dtype=np.float32)
 
+
 def search(query, top_k=3):
     # 쿼리 벡터화
     query_embedding = get_embedding(query).reshape(1, -1)
-    
+
     # 차원 일치 여부 확인
     if query_embedding.shape[1] != index_dimension:
         raise ValueError(f"Query embedding dimension {query_embedding.shape[1]} does not match index dimension {index_dimension}")
 
     # FAISS 인덱스에서 유사한 문서 검색
     D, I = index.search(query_embedding, top_k)
-    
+
     results = [(file_paths[i], documents[i]) for i in I[0]]
     return results
 
-def generate_answer(query, top_k=2):
+
+def generate_answer(query, conversation_history, top_k=2):
     # 관련 문서 검색
     relevant_docs = search(query, top_k)
-    
+
     # 관련 문서들을 하나의 문자열로 결합
     context = "\n\n".join(doc for _, doc in relevant_docs)
-    references = "\n".join(path for path, _ in relevant_docs)
-    
+    reference_links = list(set(file_path_to_url.get(path, path) for path, _ in relevant_docs))
+    references = "\n".join(reference_links)
+
     language = detect(query)
 
     if language == 'ko':
@@ -84,22 +127,28 @@ def generate_answer(query, top_k=2):
                           "it should respond as follows: [Sorry, this information is not available in the GIST regulations database. Please contact the relevant department for assistance. "
                           "(GIST 기획팀 황인호 팀장 062-715-2971)] Additionally, if the user's question is incomplete, the chatbot should request additional information based on the database. "
                           "For example: [To provide you with a more accurate answer, I need some additional information. Could you please provide details such as [necessary additional information]?")
+    conversation_history.append({"role": "user", "content": query})
 
     # OpenAI GPT-4 모델을 사용하여 답변 생성
     response = openai.ChatCompletion.create(
-        model="gpt-4",
+        model="gpt-4o",
         messages=[
             {"role": "system", "content": system_message},
+            *conversation_history,
             {"role": "user", "content": f"Here are some documents:\n\n{context}\n\nQuestion: {query}\nAnswer:"}
         ],
         max_tokens=1000  # max_tokens 값을 증가시켜 잘리는 문제 방지
     )
-    
+
     answer = response.choices[0].message['content'].strip()
-    return answer, references
+    conversation_history.append({"role": "assistant", "content": answer})
+    return answer, references, conversation_history
+
+
+conversation_history = []
 
 # Streamlit 앱 설정
-st.image(r"..\gist.jpg", use_column_width=True)
+st.image(r"..\poligi.png", use_column_width=True)
 st.header("🤖 Gist Policy App (Demo)")
 
 if 'generated' not in st.session_state:
@@ -113,14 +162,15 @@ with st.form('form', clear_on_submit=True):
     submitted = st.form_submit_button('전송')
 
 if submitted and user_input:
-    answer, references = generate_answer(user_input)
+    answer, references, conversation_history = generate_answer(user_input, conversation_history)
     st.session_state.past.append(user_input)
     st.session_state.generated.append((answer, references))
 
 if st.session_state['generated']:
-    for i in range(len(st.session_state['generated'])-1, -1, -1):
+    for i in range(len(st.session_state['generated']) - 1, -1, -1):
         user_question = st.session_state['past'][i]
         bot_answer, references = st.session_state['generated'][i]
         st.write(f'**질문:** {user_question}')
         st.write(f'**답변:** {bot_answer}')
         st.write(f'**참조한 문서 경로:**\n{references}')
+        st.markdown("---")  
